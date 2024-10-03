a2c43561c874dcffee5e852ac406f64ffb9e2e94
joaquin9830 <joaquin9811@gmail.com>
2024-10-03 11:09:02 -0300

    Comienzo de perfil

--- frontend-trooper/src/app/feature/usuario-module/usuario.module.ts
+++ frontend-trooper/src/app/feature/usuario-module/usuario.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsuarioRoutingModule } from './usuario-routing.module';
+import { PerfilUsuarioComponent } from './componentes/perfil-usuario/perfil-usuario.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PerfilUsuarioComponent
+  ],
   imports: [
     CommonModule,
     UsuarioRoutingModule
