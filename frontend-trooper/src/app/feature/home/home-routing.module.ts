9bf051cb14bc19ad65e2a1912c5ab257b1dca0d6
joaquin9830 <joaquin9811@gmail.com>
2024-10-04 16:54:39 -0300

    Estructura HTML estilos CSS de about | Enrutamiento en modulo

--- frontend-trooper/src/app/feature/home/home-routing.module.ts
+++ frontend-trooper/src/app/feature/home/home-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './componentes/about/about.component';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 
 const routes: Routes = [
   {path:'', component: InicioComponent},
-  {path:'contacto', component: ContactoComponent}
+  {path:'contacto', component: ContactoComponent},
+  {path: 'acerca-de', component: AboutComponent}
   
 ];
 
