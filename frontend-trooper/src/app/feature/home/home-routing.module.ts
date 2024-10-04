5e3281c69f2a903d30bf86f74c5e497755447dc4
joaquin9830 <joaquin9811@gmail.com>
2024-10-04 17:16:39 -0300

    Estilos de about

--- frontend-trooper/src/app/feature/home/home-routing.module.ts
+++ frontend-trooper/src/app/feature/home/home-routing.module.ts
@@ -7,7 +7,7 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
 const routes: Routes = [
   {path:'', component: InicioComponent},
   {path:'contacto', component: ContactoComponent},
-  {path: 'acerca-de', component: AboutComponent}
+  {path: 'acerca', component: AboutComponent}
   
 ];
 
