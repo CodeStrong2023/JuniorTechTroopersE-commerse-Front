10c206cd25a0d9cfe281ba409b5ae4544930cd4a
joaquin9830 <joaquin9811@gmail.com>
2024-10-09 21:18:06 -0300

    Comenzando con servicios de auth

--- frontend-trooper/src/app/shared/services/auth/auth.service.spec.ts
+++ frontend-trooper/src/app/shared/services/auth/auth.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CarritoService } from './carrito.service';
+import { AuthService } from './auth.service';
 
-describe('CarritoService', () => {
-  let service: CarritoService;
+describe('AuthService', () => {
+  let service: AuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CarritoService);
+    service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
