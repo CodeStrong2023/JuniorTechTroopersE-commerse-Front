f86b1a2d99a60ef1137b09304d5fd32420559685
joaquin9830 <joaquin9811@gmail.com>
2024-10-11 18:47:19 -0300

    Iniciando configuración de firebase storage

--- frontend-trooper/src/app/app.module.ts
+++ frontend-trooper/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';;
-import { environment } from '../environments/enviroment';
-import { AngularFireModule } from '@angular/fire';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';;
+
 
 
 @NgModule({
@@ -23,7 +24,9 @@ import { AngularFireModule } from '@angular/fire';
     
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp({"projectId":"trooperstay","appId":"1:720990543501:web:416617640bb3665f240cc4","storageBucket":"trooperstay.appspot.com","locationId":"us-central","apiKey":"AIzaSyBNRXWH1O0pP0nH701Ja3LmmVqr7dqyXBY","authDomain":"trooperstay.firebaseapp.com","messagingSenderId":"720990543501","measurementId":"G-KR06K737NR"})),
+    provideStorage(() => getStorage())
   ],
   bootstrap: [AppComponent]
 })
