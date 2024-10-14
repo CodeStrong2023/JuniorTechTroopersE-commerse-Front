2ed6ec6ffd9f58d5a5a5691fec23d42087ee7b81
joaquin9830 <joaquin9811@gmail.com>
2024-10-14 12:28:43 -0300

    Configuración de firebase funcional

--- frontend-trooper/src/enviroments/enviroment.ts
+++ frontend-trooper/src/enviroments/enviroment.ts
@@ -2,12 +2,14 @@
 //Configuración de las variables de entorno para el entorno de desarrollo
 export const environment = {
     firebase: {
-        "projectId": "trooperstay",
-        "appId": "1:720990543501:web:416617640bb3665f240cc4",
-        "storageBucket": "trooperstay.appspot.com",
-        "apiKey": "AIzaSyBNRXWH1O0pP0nH701Ja3LmmVqr7dqyXBY",
-        "authDomain": "trooperstay.firebaseapp.com",
-        "messagingSenderId": "720990543501",
-        "measurementId": "G-KR06K737NR"
+        apiKey: "AIzaSyDRGz14sohYuP3A0spsJrzIgHoLq5HASIk",
+        authDomain: "trooper-stay.firebaseapp.com",
+        projectId: "trooper-stay",
+        storageBucket: "trooper-stay.appspot.com",
+        messagingSenderId: "52197168029",
+        appId: "1:52197168029:web:e237e1341e71e458955574",
+        measurementId: "G-HH5PB5P4NK"
+
+
     }
 }
