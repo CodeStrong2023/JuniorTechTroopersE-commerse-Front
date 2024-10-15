edfccda5fcce34bfa0bdd50a7e6bca95da490d96
joaquin9830 <joaquin9811@gmail.com>
2024-10-15 10:33:16 -0300

    Servicio de home para traer datos de hospedajes

--- frontend-trooper/src/app/shared/services/home/home.service.ts
+++ frontend-trooper/src/app/shared/services/home/home.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Home } from '../../model/home';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/home';
+
+  constructor(private http: HttpClient) {}
+
+  getHospedajes(): Observable<Home[]> {
+    return this.http.get<Home[]>(this.apiUrl);
+  }
 }
