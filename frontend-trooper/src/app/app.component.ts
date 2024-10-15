282c1c15d4549cf50f63d03fbd3d0d104e05c8ec
joaquin9830 <joaquin9811@gmail.com>
2024-10-15 15:55:20 -0300

    Animaciones de AOS

--- frontend-trooper/src/app/app.component.ts
+++ frontend-trooper/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend-trooper';
+  title = 'frontend-trooper';  
+
+  ngOnInit() {
+    AOS.init();
+    window.addEventListener('load', AOS.refresh);
+  }
 }
