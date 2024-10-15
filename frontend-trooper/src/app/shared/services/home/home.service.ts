91cba7d438c7f954661240e045ce1efcf56e6bab
joaquin9830 <joaquin9811@gmail.com>
2024-10-15 10:38:32 -0300

    Documentación de servicios

--- frontend-trooper/src/app/shared/services/home/home.service.ts
+++ frontend-trooper/src/app/shared/services/home/home.service.ts
@@ -6,13 +6,19 @@ import { Home } from '../../model/home';
 @Injectable({
   providedIn: 'root'
 })
+
+// Servicio que se encarga de obtener los hospedajes de la sección de inicio
 export class HomeService {
 
+  //Variable que almacena la URL de la API
   private apiUrl = 'http://localhost:8080/home';
 
+  //Constructor que inyecta el servicio HttpClient
   constructor(private http: HttpClient) {}
 
+  //Método que obtiene los hospedajes de la sección de inicio
   getHospedajes(): Observable<Home[]> {
+    //Retorna un observable con los hospedajes
     return this.http.get<Home[]>(this.apiUrl);
   }
 }
--- frontend-trooper/src/app/shared/services/images-service/images.service.ts
+++ frontend-trooper/src/app/shared/services/images-service/images.service.ts
@@ -9,22 +9,27 @@ import { Storage } from '@angular/fire/storage';
 // Servicio para subir imágenes a Firebase Storage
 export class ImagesService {
 
+  // Constructor que inyecta el servicio de almacenamiento de Firebase
   constructor(private storage: Storage) {}
 
+  // Método para subir una imagen a Firebase Storage
   uploadImage(file: File): Promise<string> {
+    // Retorna una promesa que resuelve la URL de descarga de la imagen
     return new Promise((resolve, reject) => {
+      // Genera un nombre único para la imagen
       const filePath = `images/${Date.now()}_${file.name}`;
       const storageRef = ref(this.storage, filePath);
       const uploadTask = uploadBytesResumable(storageRef, file);
 
+      // Escucha los cambios en el estado de la subida
       uploadTask.on('state_changed', 
-        (snapshot) => {
-          // Puedes manejar el progreso de la subida si lo deseas
+        (snapshot) => {          
         }, 
         (error) => reject(error), 
         () => {
+          // Obtiene la URL de descarga de la imagen
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            resolve(downloadURL); // Retorna la URL de descarga
+            resolve(downloadURL);
           });
         }
       );
