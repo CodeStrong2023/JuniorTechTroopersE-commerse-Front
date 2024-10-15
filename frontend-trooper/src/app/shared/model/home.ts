8d886af89f3e2c12ce97d50619e64194bd03b86e
joaquin9830 <joaquin9811@gmail.com>
2024-10-15 10:32:00 -0300

    Creando modelo de home para hospedajes en tendencia

---
+++ frontend-trooper/src/app/shared/model/home.ts
@@ -0,0 +1,9 @@
+
+// Interface que define el modelo de datos de un hospedaje para la sección de inicio
+export interface Home {
+    nombreHospedaje: string;
+    description: string;
+    locality: string;
+    img_url: string;
+  }
+  
