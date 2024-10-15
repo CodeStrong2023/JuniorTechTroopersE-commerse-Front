d4d8ce69226f8fa82c0f3884f4918381b710bc44
joaquin9830 <joaquin9811@gmail.com>
2024-10-15 15:33:50 -0300

    Nuevos estilos de footer | redes sociales | terminos de uso

--- frontend-trooper/src/app/core-module/componentes/footer/footer.component.ts
+++ frontend-trooper/src/app/core-module/componentes/footer/footer.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  isTermsModalOpen: boolean = false;
+  termsAccepted: boolean = false;
 
+  // Método para abrir el modal de Términos y Condiciones
+  openTermsModal() {
+    this.isTermsModalOpen = true;
+  }
+
+  // Método para verificar si el checkbox de términos fue marcado
+  onTermsChecked(event: any) {
+    this.termsAccepted = event.target.checked;
+  }
+
+  // Método para aceptar los términos y cerrar el modal
+  acceptTerms() {
+    if (this.termsAccepted) {
+      this.isTermsModalOpen = false;
+      alert('Has aceptado los términos y condiciones.');
+    }
+  }
 }
