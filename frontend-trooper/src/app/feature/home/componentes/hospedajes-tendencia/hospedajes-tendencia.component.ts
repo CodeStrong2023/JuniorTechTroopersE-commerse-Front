d486d5ddcf56176e50cd32b46b19d6567039e2c3
joaquin9830 <joaquin9811@gmail.com>
2024-10-21 18:31:58 -0300

    Cofigurando home para traer la información de tendencias

--- frontend-trooper/src/app/feature/home/componentes/hospedajes-tendencia/hospedajes-tendencia.component.ts
+++ frontend-trooper/src/app/feature/home/componentes/hospedajes-tendencia/hospedajes-tendencia.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Home } from '../../../../shared/model/home';
+import { HomeService } from '../../../../shared/services/home/home.service';
 
 @Component({
   selector: 'app-hospedajes-tendencia',
@@ -6,31 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './hospedajes-tendencia.component.css'
 })
 export class HospedajesTendenciaComponent implements OnInit {
+  hospedajes: Home[] = [];
+
+  constructor(private homeService: HomeService) {}
+
+  ngOnInit(): void {
+    this.getHospedajes();
+  }
   
-  hospedajes = [
-    {
-      titulo: 'Cabaña Montaña Verde',
-      descripcion: 'San Rafael | Disfruta de vistas panorámicas a la montaña desde nuestra encantadora cabaña.',
-      imagenUrl: '../../../../../assets/images/tendencias/cabania.jpg',
-      url: '/detalles/cabana-montana-verde'
-    },
-    {
-      titulo: 'Cabaña Montaña Verde',
-      descripcion: 'San Rafael | Disfruta de vistas panorámicas a la montaña desde nuestra encantadora cabaña.',
-      imagenUrl: '../../../../../assets/images/tendencias/cabania.jpg',
-      url: '/detalles/cabana-montana-verde'
-    },
-    {
-      titulo: 'Cabaña Montaña Verde',
-      descripcion: 'San Rafael | Disfruta de vistas panorámicas a la montaña desde nuestra encantadora cabaña.',
-      imagenUrl: '../../../../../assets/images/tendencias/cabania.jpg',
-      url: '/detalles/cabana-montana-verde'
-    },
-  ];
-
-  constructor() { }
-
-  ngOnInit(): void {}
+   // Método para obtener los hospedajes desde el servicio
+   getHospedajes(): void {
+    this.homeService.getHospedajes().subscribe((data: Home[]) => {
+      this.hospedajes = data.slice(0, 3); // Limitar a 3 hospedajes
+    });
+  }
 
   visitarHospedaje(url: string) {
     // Lógica para redirigir a la página de detalles
