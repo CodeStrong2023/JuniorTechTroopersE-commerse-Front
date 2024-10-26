0a9b68f6c740115d8e344a6351aa44b91bf2d25a
joaquin9830 <joaquin9811@gmail.com>
2024-10-26 17:47:19 -0300

    Probando configuración de APIS

--- frontend-trooper/src/app/shared/services/auth/auth.service.ts
+++ frontend-trooper/src/app/shared/services/auth/auth.service.ts
@@ -10,14 +10,9 @@ import { tap } from 'rxjs/operators';
 // Servicio que se encarga de la autenticación de los usuarios (registro, login, perfil y logout)
 export class AuthService {
 
-  // Variables que almacenan las URLs de la API de DESARROLLO
-  //private baseUrl = 'http://localhost:8080/auth';
-  //private profileUrl = 'http://localhost:8080/users/perfil';
-
-
-  // Variables que almacenan las URLs de la API de PRODUCCIÓN
-  private baseUrl='https://login-o23e.onrender.com/auth';
-  private profileUrl='https://login-o23e.onrender.com/users/perfil';
+  // Variables que almacenan las URLs de la API
+  private baseUrl = 'http://localhost:8080/auth';
+  private profileUrl = 'http://localhost:8080/users/perfil';
   // Variable que almacena el token del usuario
   private token: string | null = null;
 
