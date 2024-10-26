45784a2c1e8a0b8db65abb863ce48219b1cafd42
joaquin9830 <joaquin9811@gmail.com>
2024-10-26 10:30:43 -0300

    Configurando ruteo de hospedaje-tendencia | hospedaje especifico | servicio de home con token

--- frontend-trooper/src/app/shared/model/home.ts
+++ frontend-trooper/src/app/shared/model/home.ts
@@ -1,6 +1,7 @@
 
 // Interface que define el modelo de datos de un hospedaje para la sección de inicio
 export interface Home {
+    hospedajeToken: string;
     nombreHospedaje: string;
     description: string;
     locality: string;
