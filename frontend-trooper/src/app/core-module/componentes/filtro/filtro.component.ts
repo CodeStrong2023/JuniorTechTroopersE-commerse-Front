e155211c8796c33e729d496512cb23a8b595a0d7
joaquin9830 <joaquin9811@gmail.com>
2024-10-26 10:44:58 -0300

    Implementando lógica para el filtrado de hospedaje | filtro.ts

--- frontend-trooper/src/app/core-module/componentes/filtro/filtro.component.ts
+++ frontend-trooper/src/app/core-module/componentes/filtro/filtro.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FiltroService } from '../../../shared/services/filtro.service';
+import { Hospedaje } from '../../../shared/model/hospedaje';
+import { HospedajeService } from '../../../shared/services/hospedaje/hospedaje.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-filtro',
@@ -9,8 +12,13 @@ import { FiltroService } from '../../../shared/services/filtro.service';
 export class FiltroComponent {
   suggestions: any[] = []; // Almacena las sugerencias
   selectedLocation: any = null; // La ubicación seleccionada
+  selectedDate: string = ''; // La fecha seleccionada
 
-  constructor(private filtroService: FiltroService) { }
+  constructor(
+    private filtroService: FiltroService,
+    private hospedajeService: HospedajeService,
+    private router: Router
+    ) { }
 
   // Método que busca localizaciones cuando el usuario escribe
   onSearchLocation(event: Event) {
@@ -35,5 +43,37 @@ export class FiltroComponent {
     this.selectedLocation = suggestion; // Guardar la localización seleccionada
     console.log('Selected location:', suggestion);
     this.suggestions = []; // Limpiar sugerencias después de seleccionar
+  }
+
+  // Método para capturar la fecha seleccionada
+  onSelectDate(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.selectedDate = input.value;
+  }
+
+  // Método para buscar hospedajes con los filtros
+  onSearch() {
+    if (this.selectedLocation && this.selectedDate) {
+      const locality = this.selectedLocation.nombre;
+      const date = this.selectedDate;
+
+      // Si estamos en la sección de /hospedajes, realizamos la búsqueda sin redirigir
+      if (this.router.url === '/hospedajes') {
+        this.hospedajeService.getHospedajesFiltrados(locality, date).subscribe(
+          (data) => {
+            // Manejar los resultados de la búsqueda aquí
+            console.log('Filtered hospedajes:', data);
+          },
+          (error) => {
+            console.error('Error fetching filtered hospedajes', error);
+          }
+        );
+      } else {
+        // Redirigir a la sección de /hospedajes con los parámetros de búsqueda
+        this.router.navigate(['/hospedajes'], { queryParams: { locality, date } });
+      }
+    } else {
+      console.error('Por favor, selecciona una ubicación y una fecha');
+    }
   }
 }
