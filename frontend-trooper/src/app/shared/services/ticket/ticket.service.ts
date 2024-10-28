b037ff32335d68e8eb96f5fba3d2676b23d8f35f
joaquin9830 <joaquin9811@gmail.com>
2024-10-28 09:35:04 -0300

    Configurando ticket

--- frontend-trooper/src/app/shared/services/ticket/ticket.service.ts
+++ frontend-trooper/src/app/shared/services/ticket/ticket.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Ticket } from '../../model/ticket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketService {
+  private apiUrl = 'http://localhost:8080/ticket';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  generateTicket(ticketData: Ticket, userToken: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${userToken}`,
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post(this.apiUrl, ticketData, { headers });
+  }
 }
