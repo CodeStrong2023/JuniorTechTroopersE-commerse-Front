f64b395dd1129a10dde1d75825415492d1297928
joaquin9830 <joaquin9811@gmail.com>
2024-10-28 19:49:56 -0300

    Configurando logica para la card emergente de reservas

--- frontend-trooper/src/app/feature/usuario-module/componentes/detalle-ticket/detalle-ticket.component.ts
+++ frontend-trooper/src/app/feature/usuario-module/componentes/detalle-ticket/detalle-ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TicketResponseDTO } from '../../../../shared/model/ticket-DTO';
 
 @Component({
   selector: 'app-detalle-ticket',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './detalle-ticket.component.css'
 })
 export class DetalleTicketComponent {
+  @Input() ticketDetail!: TicketResponseDTO; // Detalles de la reserva/alojamiento
+  @Output() close = new EventEmitter<void>(); // Evento para cerrar el modal
 
+  //Método para cerrar la card
+  closeModal() {
+    this.close.emit();
+  }
 }
